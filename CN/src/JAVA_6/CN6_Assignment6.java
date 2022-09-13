4b230f8c63c0908ffb2a9f78ba09fd2fe6a678a2
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-09-13 22:29:57 +0530

    New Lecture

---
+++ CN/src/JAVA_6/CN6_Assignment6.java
@@ -0,0 +1,42 @@
+/*
+Assignment Coding Problem
+Problem Name: Binary to decimal
+Problem Level: MEDIUM
+Problem Description: ####Given a binary number as an integer N, convert it into decimal and print.
+
+#####Input format :
+    An integer N in the Binary Format
+
+#####Output format :
+    Corresponding Decimal number (as integer)
+
+##### Constraints :
+    0 <= N <= 10^9
+
+ #####Sample Input 1 :
+    1100
+
+#####Sample Output 1 :
+    12
+
+#####Sample Input 2 :
+    111
+
+#####Sample Output 2 :
+    7
+ */
+
+package JAVA_6;
+
+import java.util.Scanner;
+
+public class CN6_Assignment6 {
+    public static void main(String[] args) {
+    Scanner read = new Scanner(System.in);
+    int num = read.nextInt();
+    int j=0;
+    for (int i=0;i<num/10;i++){
+
+    }
+    }
+}
