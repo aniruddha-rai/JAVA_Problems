58ca9e27140bdc6ced2fc871657306898c1918c3
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-09-14 22:00:28 +0530

    Assignment Modified

--- CN/src/JAVA_6/CN6_BitwiseOperator.java
+++ CN/src/JAVA_6/CN6_BitwiseOperator.java
@@ -2,6 +2,12 @@ package JAVA_6;
 
 public class CN6_BitwiseOperator {
     public static void main(String[] args) {
+
+        System.out.println(2 & 3);  // Bitwise AND
+        System.out.println(2 | 3);  // Bitwise OR
+        System.out.println(2 ^ 3);  // Bitwise XOR
+        System.out.println(~2);     // Bitwise NOT
+
         int a = 42;
         int b = ~a; //(there is tilde sign before a)
         System.out.print(a + " " + b);
