7e76a716157fc303552c991426cbac03cc8db8ce
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-09-14 22:13:10 +0530

    Code Modified

--- CN/src/JAVA_6/CN6_BitwiseOperator.java
+++ CN/src/JAVA_6/CN6_BitwiseOperator.java
@@ -8,6 +8,11 @@ public class CN6_BitwiseOperator {
         System.out.println(2 ^ 3);  // Bitwise XOR
         System.out.println(~2);     // Bitwise NOT
 
+        System.out.println(34 << 1);    // Left Shift on Positive no
+        System.out.println(34 >> 2);    // Right Shift on Positive no
+        System.out.println(-4 << 1);    // Left Shift on Negative no
+        System.out.println(-4 >> 1);    // Right Shift on Negative no
+
         int a = 42;
         int b = ~a; //(there is tilde sign before a)
         System.out.print(a + " " + b);
