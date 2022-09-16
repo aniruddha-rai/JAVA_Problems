d1065769421bfb384f509d8528170aabfc21c489
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-09-17 00:16:22 +0530

    New Lecture Added

---
+++ CN/src/JAVA_8_Arrays/CN8_1_Array.java
@@ -0,0 +1,20 @@
+package JAVA_8_Arrays;
+
+import java.util.Scanner;
+
+public class CN8_1_Array {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+//        arr[3] = 15;
+        System.out.println(arr[3]);
+
+        char[] chArr = new char[10];
+        System.out.println(chArr[3]);
+
+        double[] dArr = new double[10];
+        System.out.println(dArr[5]);
+
+        boolean bArr[] = new boolean[5];
+        System.out.println(bArr[0]);
+    }
+}
