a15e7c70d06fca13ca16e5da5349a2eda21ba700
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-09-23 00:12:16 +0530

    New Lecture Added

--- CN/src/JAVA_9_Searching_Sorting/CN9_5_MergeTwoSortedArray.java
+++ CN/src/JAVA_9_Searching_Sorting/CN9_5_MergeTwoSortedArray.java
@@ -5,20 +5,43 @@ import java.util.Scanner;
 public class CN9_5_MergeTwoSortedArray {
 
     public static void mergeArray(int[] arr1, int[] arr2, int size1, int size2){
-        int[] arr3 = new int[arr1.length+arr2.length];
-        int i=0, j=0;
-        for (int k=0;k<arr3.length;k++){
-                if (arr1[i] >= arr2[j] && j<size2-1) {
-                    arr3[k] = arr2[j];
-                    j++;
-                } else if (arr1[i] <= arr2[j] && i<size1-1) {
-                    arr3[k] = arr1[i];
-                    i++;
-                }else if (i == size1-1){
-                    arr3[k] = arr2[j];
-                } else if (j == size2-1) {
-                    arr3[k] = arr1[i];
-                }
+//        int[] arr3 = new int[size1+size2];
+//        int i=0, j=0;
+//        for (int k=0;k<arr3.length;k++){
+//                if (arr1[i] >= arr2[j] && j<size2-1) {
+//                    arr3[k] = arr2[j];
+//                    j++;
+//                } else if (arr1[i] <= arr2[j] && i<size1-1) {
+//                    arr3[k] = arr1[i];
+//                    i++;
+//                }else if (i < size1){
+//                    arr3[k] = arr2[j];
+//                } else if (j < size2) {
+//                    arr3[k] = arr1[i];
+//                }
+//        }
+        int i=0,j=0,k=0;
+        int[] arr3 = new int[size1+size2];
+        while (i<size1 && j<size2){
+            if (arr1[i] < arr2[j]){
+                arr3[k] = arr1[i];
+                i++;
+                k++;
+            }else {
+                arr3[k] = arr2[j];
+                j++;
+                k++;
+            }
+        }
+        while (i<size1){
+            arr3[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while (j<size2){
+            arr3[k] = arr2[j];
+            j++;
+            k++;
         }
         printArray(arr3);
     }
