2f0cacda67b7450bd98550a74f263e0990c4b56e
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-09-26 22:37:06 +0530

    New Lecture Added

--- CN/src/JAVA_10_Strings/CN10_8_ReverseStringWordWise.java
+++ CN/src/JAVA_10_Strings/CN10_8_ReverseStringWordWise.java
@@ -1,3 +1,34 @@
+/*
+Coding Problem
+Problem Name: Reverse string Word Wise
+Problem Level: MEDIUM
+Problem Description: #### Reverse the given string word wise. That is, the last word in given string should come at 1st place, last second word at 2nd place and so on. Individual words should remain as it is.
+
+##### Input format :
+    String in a single line
+##### Output format :
+    Word wise reversed string in a single line
+##### Constraints :
+    0 <= |S| <= 10^7
+    where |S| represents the length of string, S.
+ ##### Sample Input 1:
+
+    Welcome to Coding Ninjas
+
+##### Sample Output 1:
+
+    Ninjas Coding to Welcome
+
+##### Sample Input 2:
+
+    Always indent your code
+
+##### Sample Output 2:
+    code your indent Always
+
+
+ */
+
 package JAVA_10_Strings;
 
 import java.util.Scanner;
@@ -28,6 +59,5 @@ public class CN10_8_ReverseStringWordWise {
                 end = print(input, start, end);
             }
         }
-//        System.out.println(count);
     }
 }
