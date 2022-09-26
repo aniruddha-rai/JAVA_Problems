bff856fbf700c1372c95694069d54064cb7baf1f
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-09-27 00:09:58 +0530

    New Lecture Added

--- CN/src/JAVA_10_Strings/CN10_Assignment5_HighestOccuringCharacter.java
+++ CN/src/JAVA_10_Strings/CN10_Assignment5_HighestOccuringCharacter.java
@@ -37,17 +37,18 @@ public class CN10_Assignment5_HighestOccuringCharacter {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         String character = read.nextLine();
-        int count = 1;
-        char ch;
+//        int count = 1;
+        char ch = 0;
         for (int i=0;i<character.length();i++){
             for (int j=i+1;j<character.length();j++){
-                if (character.charAt(i) == character.charAt(j)){
-                    count++;
+                if (character.charAt(i) == character.charAt(j)) {
+//                    count++;
                     ch = character.charAt(i);
+                    break;
                 }
             }
-            count = 0;
+//            count = 0;
         }
-        System.out.println(count);
+        System.out.println(ch);
     }
 }
