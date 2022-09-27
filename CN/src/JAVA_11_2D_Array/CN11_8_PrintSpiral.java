cd993832c7fc1a69d93ecf6bd9c89d70f38c9453
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-09-28 00:06:06 +0530

    New Lecture Added

---
+++ CN/src/JAVA_11_2D_Array/CN11_8_PrintSpiral.java
@@ -0,0 +1,97 @@
+/*
+Coding Problem
+Problem Name: Print Spiral
+Problem Level: HARD
+Problem Description:
+#### For a given two-dimensional integer array/list of size (N x M), print it in a spiral form. That is, you need to print in the order followed for every iteration:
+    a. First row(left to right)
+    b. Last column(top to bottom)
+    c. Last row(right to left)
+    d. First column(bottom to top)
+
+#### Mind that every element will be printed only once.
+
+
+##### Refer to the Image:
+![Alt txt](https://files.codingninjas.in/0000000000004006.jpeg)
+
+
+##### Input format :
+    The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+    First line of each test case or query contains two integer values, 'N' and 'M', separated by a single space. They represent the 'rows' and 'columns' respectively, for the two-dimensional array/list.
+
+    Second line onwards, the next 'N' lines or rows represent the ith row values.
+
+    Each of the ith row constitutes 'M' column values separated by a single space.
+
+
+##### Output format :
+    For each test case, print the elements of the two-dimensional array/list in the spiral form in a single line, separated by a single space.
+
+    Output for every test case will be printed in a seperate line.
+
+##### Constraints :
+    1 <= t <= 10^2
+    0 <= N <= 10^3
+    0 <= M <= 10^3
+    Time Limit: 1sec
+ ##### Sample Input 1:
+    1
+    4 4
+    1 2 3 4
+    5 6 7 8
+    9 10 11 12
+    13 14 15 16
+
+##### Sample Output 1:
+    1 2 3 4 8 12 16 15 14 13 9 5 6 7 11 10
+
+##### Sample Input 2:
+    2
+    3 3
+    1 2 3
+    4 5 6
+    7 8 9
+    3 1
+    10
+    20
+    30
+
+##### Sample Output 2:
+    1 2 3 6 9 8 7 4 5
+    10 20 30
+ */
+
+package JAVA_11_2D_Array;
+
+import java.util.Scanner;
+
+public class CN11_8_PrintSpiral {
+
+    public static void printSpiral(int[][] arr){
+        for (int i=0;i<arr.length;i++){
+            for (int j=0;j<arr[i].length;j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+//            System.out.println(arr[i].length);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int num = read.nextInt();
+        for (int k=0;k<num;k++) {
+            int row = read.nextInt();
+            int column = read.nextInt();
+            int[][] arr = new int[row][column];
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < column; j++) {
+                    arr[i][j] = read.nextInt();
+                }
+            }
+            printSpiral(arr);
+        }
+    }
+}
