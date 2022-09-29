8b37dd2b7d93ff5df184a9824a6df5ee90de8b0d
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-09-29 22:55:55 +0530

    New Lecture Added

--- CN/src/JAVA_11_2D_Array/CN11_10_Other.java
+++ CN/src/JAVA_11_2D_Array/CN11_10_Other.java
@@ -1,7 +1,7 @@
 package JAVA_11_2D_Array;
 
-public class CN11_10_Others {
+public class CN11_10_Other {
     public static void main(String[] args) {
-
+        System.out.println("Other");
     }
 }
