dad918a385073f423ce1a8645cbd75433574556c
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-09-30 17:11:43 +0530

    New Lecture Added

--- CN/src/JAVA_9_Searching_Sorting/CN9_3_BubbleSort.java
+++ CN/src/JAVA_9_Searching_Sorting/CN9_3_BubbleSort.java
@@ -52,11 +52,13 @@ import java.util.Scanner;
 
 public class CN9_3_BubbleSort {
 
+    // Time Complexity = O(n^2)
     public static void bubbleSort(int[] arr){
         int temp = 0;
         for (int i=0;i<arr.length-1;i++){
             for (int j=0;j<arr.length-1;j++) {
                 if (arr[j] > arr[j + 1]) {
+                    // Swap
                     temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
