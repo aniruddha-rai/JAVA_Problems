13b19c4c69da400e9c8d0a40eb5650eddcd10e9b
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-10-02 16:18:13 +0530

    New Lecture Added

---
+++ CN/JAVA/JAVA_12_Temp/StudentUse2.java
@@ -0,0 +1,13 @@
+package JAVA_12_Temp;
+
+import JAVA_12_OOPS1.Student;
+// Here we have to import because we are using class from another package
+
+public class StudentUse2 {
+//    Student s = new Student();
+//    s.name = "Ani";  name id DEFAULT access modifier that means it is accessible only within the same package.
+
+//    Student s = new Student();
+    // Now name is public so that it can be used anywhere
+
+}
