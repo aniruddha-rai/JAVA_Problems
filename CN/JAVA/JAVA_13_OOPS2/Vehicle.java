57f55758a94e91bb2013905a3ffcc41301bbe1a7
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-10-08 03:53:55 +0530

    New Lecture Added

--- CN/JAVA/JAVA_13_OOPS2/Vehicle.java
+++ CN/JAVA/JAVA_13_OOPS2/Vehicle.java
@@ -1,7 +1,8 @@
 package JAVA_13_OOPS2;
 
+// Protected ---> Same as Default/Friendly but will allow in other packages too which has sub class like we have in TRUCK Class
 public class Vehicle {
-    String color;
+    protected String color;
     private int maxSpeed;
 
     public int getMaxSpeed(){
