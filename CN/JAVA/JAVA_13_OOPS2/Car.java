63dfb617e1612a3ceb7dc22ad79bd7e05b82ae09
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-10-09 23:14:21 +0530

    Code Modified

--- CN/JAVA/JAVA_13_OOPS2/Car.java
+++ CN/JAVA/JAVA_13_OOPS2/Car.java
@@ -8,6 +8,14 @@ public class Car extends Vehicle {
     int numGears;
     boolean isConvertible;
 
+    // Constructor
+    public Car(int numGears, int maxSpeed){
+        // super(200); // as we don't have defualt constuctor of parent class vehicle we have define the parameter to get rid pof errors
+        super(maxSpeed);
+        this.numGears = numGears;
+        System.out.println("Car Constructor");
+    }
+
     public void print(){
 //        System.out.println("Car Color : " + color);
 //        System.out.println("Car Max Speed : " + getMaxSpeed());
