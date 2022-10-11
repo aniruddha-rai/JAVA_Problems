765fa46cd6bfb1d5850d01a99d4a4def23b77c88
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-10-11 22:54:23 +0530

    New Code Added

---
+++ CN/JAVA/JAVA_14_Recursion/CN14_2_SumOfN_NaturalNo.java
@@ -0,0 +1,17 @@
+package JAVA_14_Recursion;
+
+public class CN14_2_SumOfN_NaturalNo {
+
+    public static int sum(int n){
+        if (n == 1){
+            return 1;
+        }
+        return n+sum(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        int ans = sum(n);
+        System.out.println(ans);
+    }
+}
