ecd5633107464c089349af2299e4968b6ebf7afa
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-10-13 00:01:20 +0530

    New Code Added

--- CN/JAVA/JAVA_14_Recursion/CN14_8_CheckSorted.java
+++ CN/JAVA/JAVA_14_Recursion/CN14_8_CheckSorted.java
@@ -24,8 +24,34 @@ public class CN14_8_CheckSorted {
 
     }
 
+
+    public static boolean checkSorted_2(int[] arr){
+        if (arr.length <= 1){
+            return true;
+        }
+        // Checking first and second element so that we don't need to wait till the n-1 elements
+        if (arr[0] > arr[1]){
+            return false;
+        }
+
+        int[] smallArr = new int[arr.length - 1];
+        for (int i=1;i<arr.length;i++){
+            smallArr[i-1] = arr[i];
+        }
+
+        boolean smallAns = checkSorted_2(smallArr);
+        return smallAns;
+//        if (smallAns){
+//            return true;
+//        }else {
+//            return false;
+//        }
+
+    }
+
     public static void main(String[] args) {
         int[] arr = {1,3,5,6,9};
         System.out.println(checkSorted(arr));
+        System.out.println(checkSorted_2(arr));
     }
 }
