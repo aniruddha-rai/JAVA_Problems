68f20111ee80e33ef04a6398265850a50843e6ee
aniruddha-rai <aniruddha7.cr@gmail.com>
2022-11-02 23:17:05 +0530

    New Code Added

--- CN/JAVA/JAVA_14_Recursion/CN14_9_SumOfArrayElements.java
+++ CN/JAVA/JAVA_14_Recursion/CN14_9_SumOfArrayElements.java
@@ -2,20 +2,19 @@ package JAVA_14_Recursion;
 
 public class CN14_9_SumOfArrayElements {
 
-    public static int sum(int[] arr, int sum){
-        if (arr.length <= 1){
-            return sum += arr[0];
+    public static int sum(int[] arr, int n){
+        if (n <= 0){
+            return 0;
         }
-
-        int[] smallArr = new int[arr.length - 1];
-        for (int i=1;i<arr.length;i++){
-            smallArr[i-1] = arr[i];
-        }
-        return sum(smallArr, sum);
+//        int[] smallArr = new int[arr.length - 1];
+//        for (int i=1;i<arr.length;i++){
+//            smallArr[i-1] = arr[i];
+//        }
+        return sum(arr, n-1) + arr[n-1];
     }
 
     public static void main(String[] args) {
         int[] arr = {1,3,5};
-        System.out.println(sum(arr, 0));
+        System.out.println(sum(arr, arr.length));
     }
 }
