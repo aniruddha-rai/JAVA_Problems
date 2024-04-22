fc36a1bcdffde2354e96be510d96eb08d67b701e
aniruddha-rai <aniruddha7.cr@gmail.com>
2024-04-22 22:48:01 +0530

    Code Modified

--- CN/JAVA/JAVA_1_to_5/CN4_Pattern_Problems.java
+++ CN/JAVA/JAVA_1_to_5/CN4_Pattern_Problems.java
@@ -580,6 +580,26 @@ public class CN4_Pattern_Problems {
 //            i++;
 //        }
 
+        int i = 1;
+        while (i <= n) {
+            int j = 1;
+            while (j <= n - i) {
+                System.out.print(" ");
+                j++;
+            }
+            j = 1;
+            while (j <= i) {
+                System.out.print("*");
+                j++;
+            }
+            j = 1;
+            while (j <= i - 1) {
+                System.out.print("*");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
 
 
     }
